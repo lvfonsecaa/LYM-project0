af90e17111007bd3ed9af84f5464188c9cd97feb
lvfonsecaa <lv.fonseca@uniandes.edu.co>
2024-09-07 13:19:12 -0500

    parser??

--- model.py
+++ model.py
@@ -8,3 +8,24 @@ def lexer(texto:str):
     tokens = re.findall(r"\w+|[^\w\s]", texto)
     return tokens
 
+palabras_reservadas = ["turntomy", "turntothe", "walk", "jump", "drop", "pick", "grab", "letgo", "pop", "moves", "nop", "safeexe", "exec", "new", "var", "macro", "isblocked", "isfacing", "blocked", "facing", "zero"]
+tokens_validos =["(", ")", ",", "{", "}", ";", "?", "=", "while", "if ", "then", "not", "else", "fi", "do", "od"]
+
+def parser(tokens:list):
+    variables = []
+    macros = []
+    tokens = lexer()
+    for element in tokens:
+        if tokens[element] == "new":    
+            if tokens[element+1] == "var":
+                #mirar parentesis?
+                variables.append(tokens[element+3])
+            elif tokens[element+1] == "macro":
+                valido = []
+                #...
+                macros.append()
+            else:
+                return False
+            
+        #if 
+        
